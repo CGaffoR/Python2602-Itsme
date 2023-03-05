60cb8d7ac70ef9839732c1c991a22e70ba1e2bc2
CGaffoR <carlos.gaffo@live.com>
2023-03-05 19:45:22 -0300

    launch script.py

---
+++ script.py
@@ -0,0 +1,19 @@
+
+#Nome 
+name = 'Carlos Eduardo';
+#Sobrenome
+sobrenome = 'Gaffo da Rocha';
+#Idade
+idade = 19;
+#Data de nascimento
+data_nascimento = '26-06-2003';
+#Altura
+altura = 1.82;
+#Peso
+peso = 98.82
+
+print('===========================================\n'+
+      f'Nome: {name}\n'+f'Sobrenome: {sobrenome}\n'+
+      f'Idade: {idade}\n'+f'Data de nascimento: {data_nascimento}\n'+
+      f'Altura: {altura}\n'+f'Peso: {peso}\n'+
+      '===========================================')
